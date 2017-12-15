3bcf05788f39a2d9dbeeeb268c96aa47d7c87780
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2017-12-15 10:59:58 +0000

    Add tests for GET/ todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,15 +1,26 @@
+//import { disconnect } from 'cluster';
+
 const expect = require('expect');
 const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+},
+{
+    text:'Second test todo'
+}];
 
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(()=>done());
 })
 
 //The code above ensures we run with zero todos
+//Need to create test data, in order to test GET route 
 
 describe('POST /todos', ()=>{
     it('should create a new todo', (done)=>{
@@ -26,7 +37,7 @@ describe('POST /todos', ()=>{
             if(err){
                 return done(err)
             }
-            Todo.find().then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -44,9 +55,21 @@ describe('POST /todos', ()=>{
             return done(err);
         }
         Todo.find().then((todos)=>{
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done()
         }).catch((e)=>done(e));
     })
 })
 });
+
+describe('GET /todos', ()=>{
+    it('should get all todos', (done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2)
+        })
+        .end(done);
+    }) //don't need to add anyting like above, since we are not doing anything async
+})
