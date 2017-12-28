12b1387ca82228c723150517eb29aa220dd5df1d
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2017-12-28 01:35:30 +0000

    Add tests for PATCH /todos/id and correct DELETE tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -15,7 +15,9 @@ const todos = [{
 },
 {
     _id: new ObjectID(),
-    text:'Second test todo'
+    text:'Second test todo',
+    completed:true,
+    completedAt:333
 }];
 
 beforeEach((done)=>{
@@ -137,12 +139,54 @@ describe('DELETE /todos/:id', ()=>{
             .expect(404)
             .end(done);
      });
-     it('should retuen 404 if object id is invalid', (done)=>{
         it('should return a 404 for non object ids',(done)=>{
             request(app)
             .delete(`/todos/123`)
             .expect(404)
             .end(done);
-        })
      });
 })
+
+describe('PATCH /todos/:id',()=>{
+    it('should update the todo',(done)=>{
+        let hexId = todos[0]._id.toHexString();
+        let text ="set alarm";
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            text,
+            completed:true
+        })
+        .expect(200)
+        .expect(res=>{
+            expect(res.body.todo.text).toBe(text)
+            expect(res.body.todo.completed).toBe(true)
+            expect(res.body.todo.completedAt).toBeA('number')
+        })
+        .end(done)
+     })  
+       
+    
+        //grab id of first item
+        //update text, set completed to true
+        //200
+        //text is changed, compeleted is true, completed is a number .toBeA
+    it('should clear completedAt when todo is not completed', (done)=>{
+        let hexId = todos[1]._id.toHexString();
+        let text = "Update text";
+
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            text,
+            completed:false
+        })
+        .expect(200)
+        .expect(res=>{
+            expect(res.body.todo.text).toBe(text)
+            expect(res.body.todo.completed).toBe(false)
+            expect(res.body.todo.completedAt).toNotExist()
+        })
+        .end(done)
+    });
+});
